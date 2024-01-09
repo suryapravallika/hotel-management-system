72b65d8afc808cb0169fd0cc78ff9c1435e6b0da
suryapravallika <115685737+suryapravallika@users.noreply.github.com>
2024-01-09 20:47:47 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const mongoose =require('mongoose')
+const cors = require('cors')
+
+app.get('/',(req,res)=>{res.send("hello from server..")})
+
+app.use(cors())
+app.use(express.json())
+
+const Db = async()=>{
+    try {
+        await mongoose.connect('mongodb+srv://pravallikanaravula:pravallika1234@cluster0.ve97ibt.mongodb.net/?retryWrites=true&w=majority').then(
+            ()=>{
+                console.log("Connected to DataBase...")
+            }
+        )
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+Db();
+
+app.listen(1873,()=>{console.log("Server is running on port 5000...")})
